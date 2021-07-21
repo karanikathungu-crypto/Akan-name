7bbd373ae2bdc7281244d2be8dd7501d37ce3af2
karanikathungu-crypto <kathungudev@gmail.com>
2021-07-21 10:25:52 +0300

    Add javascript file to project

--- Javs/main.js
+++ Javs/main.js
@@ -1,27 +1,2 @@
-//variable declaration
-const myForm = document.querySelector('.dos');
+alert('Hey guys');
 
-const dobInput = document.querySelector('#dobb');
-
-const genderInput = document.querySelector('#gender');
-
-const btn = document.querySelector('.buttonn');
-btn.addEventListener('click', (e)=> 
-  {e.preventDefault();
-  console.log('click');
-})
-
-myForm.addEventListener('Submit', onSubmit);
-function onSubmit(e){
-  alert("thanks ");}
-  e.preventDefault();
-  if(dobInput.value === null || genderInput.value === null)
-   {
-      alert('Please select required fields')
-     }
-     else{
-       console.log('success');
-     }
-}
-let dob =  document.querySelector('input[type="date"]')
-  console.log(dob.value);
